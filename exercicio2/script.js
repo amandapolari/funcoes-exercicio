c96382268fd17730f8d2f8c1f7ce82ed216e2cff
Amanda <amandaoliveirapolari@gmail.com>
2023-02-08 11:30:35 -0300

    Ajustes no Exercício 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,36 +1,53 @@
 // EXERCÍCIO 2
 // 📌 a)
-// (FORMA 1) => Números chumbados em uma variável:
+// (FORMA 1) => Sem criar variáveis para os números:
+function somarNumeros(num1, num2) {
+    const resultadoSoma = num1 + num2;
+    console.log(`
+    O resultado da soma de ${num1} com ${num2} é igual a: ${resultadoSoma}
+    `);
+}
+somarNumeros(2,7);
+
+// (TREINANDO OUTRAS FORMAS)
+// (FORMA 2) => Números chumbados em uma variável:
 // const primeiroNumero = 2;
 // const segundoNumero = 5;
 
-// (FORMA 2) => Recebendo o número do usuário
+// (FORMA 3) => Recebendo o número do usuário
 // const primeiroNumero = Number(prompt('Digite seu primeiro número:'));
 // const segundoNumero = Number(prompt('Digite seu segundo número:'));
 
-// (CÓDIGO PARA AS DUAS FORMAS:)
+// (CÓDIGO PARA AS FORMAS 2 e 3)
 // function somarNumeros(num1, num2) {
 //     const resultadoDaSoma = num1 + num2;
 //     return resultadoDaSoma;
 // }
-
 // const resultadoFinalDaSoma = somarNumeros(primeiroNumero, segundoNumero);
 // console.log(`
 // O resultado da soma é igual a ${resultadoFinalDaSoma}
 // `);
 
-//=============>
-
 // 📌 b)
-// (FORMA 1) => Números chumbados em uma variável
+// (FORMA 1) => Sem criar variáveis para os números:
+function primeiroEhMaiorOuIgualAoSegundo(num1, num2) {
+    const resultadoDaComparacao = num1 > num2;
+    console.log(`
+    O número ${num1} é maior ou igual ao número ${num2}? ${resultadoDaComparacao}
+    `)
+}
+primeiroEhMaiorOuIgualAoSegundo(6,1);
+
+// (TREINANDO OUTRAS FORMAS)
+// (FORMA 2) => Números chumbados em uma variável
 // const primeiroNumero = 2;
 // const segundoNumero = 6;
 
-// (FORMA 2) => Recebendo o número do usuário
+// (FORMA 3) => Recebendo o número do usuário
 // const primeiroNumero = Number(prompt('Digite seu primeiro número:'));
 // const segundoNumero = Number(prompt('Digite seu segundo número:'));
 
-// (CÓDIGO PARA AS DUAS FORMAS:)
+// (CÓDIGO PARA AS FORMAS 2 e 3)
 // function verificarMaior(num1, num2){
 //     const primeiroMaiorQueSegundo = num1 > num2;
 //     return primeiroMaiorQueSegundo;
@@ -41,13 +58,23 @@
 // `);
 
 // 📌 c)
-// (FORMA 1) => Números chumbados em uma variável
-const numero = 4;
+// (FORMA 1) => Sem criar variáveis para os números:
+function verificarSeEhPar(num) {
+    const resultadoDaComparacao = (num % 2) === 0;
+    console.log(`
+    O número ${num} é par? ${resultadoDaComparacao}
+    `)
+}
+verificarSeEhPar(3);
 
-// (FORMA 2) => Recebendo o número do usuário
+// (TREINANDO OUTRAS FORMAS)
+// (FORMA 2) => Números chumbados em uma variável
+// const numero = 4;
+
+// (FORMA 3) => Recebendo o número do usuário
 // const numero = Number(prompt('Digite um número:'));
 
-// (CÓDIGO PARA AS DUAS FORMAS:)
+// (CÓDIGO PARA AS FORMAS 2 e 3)
 // function verificaSeEhPar(num) {
 //     const ehPar = (num % 2) === 0;
 //     return ehPar;
@@ -58,20 +85,32 @@ const numero = 4;
 // `);
 
 // 📌 d)
-// (FORMA 1) => Números chumbados em uma variável
-const salario = 3500;
-
-// (FORMA 2) => Recebendo o número do usuário
-// const salario = Number(prompt('Digite o valor do seu salário'));
-
-// (CÓDIGO PARA AS DUAS FORMAS:)
-function calcularSalarioLiquido(salario) {
-    const porcentDeDescontoInss = 0.1;
-    const valorDoDescontoInss = salario * porcentDeDescontoInss;
-    const salarioLiquido = salario - valorDoDescontoInss;
+// (FORMA 1) => Sem criar variáveis para os números:
+function calcularSalarioLiquido(salarioBruto) {
+    const desconto = salarioBruto * 0.1;
+    const salarioLiquido = salarioBruto - desconto;
     return salarioLiquido;
 }
-const resultadoFinal = calcularSalarioLiquido(salario);
+const meuSalarioLiquido = calcularSalarioLiquido(3000);
 console.log(`
-O salário líquido é igual a: R$ ${resultadoFinal}
+    O salário líquido com desconto é igual a R$ ${meuSalarioLiquido}
 `)
+
+// (TREINANDO OUTRAS FORMAS)
+// (FORMA 2) => Números chumbados em uma variável
+// const salario = 3500;
+
+// (FORMA 3) => Recebendo o número do usuário
+// const salario = Number(prompt('Digite o valor do seu salário'));
+
+// (CÓDIGO PARA AS FORMAS 2 e 3)
+// function calcularSalarioLiquido(salario) {
+//     const porcentDeDescontoInss = 0.1;
+//     const valorDoDescontoInss = salario * porcentDeDescontoInss;
+//     const salarioLiquido = salario - valorDoDescontoInss;
+//     return salarioLiquido;
+// }
+// const resultadoFinal = calcularSalarioLiquido(salario);
+// console.log(`
+// O salário líquido é igual a: R$ ${resultadoFinal}
+// `)
