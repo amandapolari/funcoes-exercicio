929e597cb1812e8100260eb037ac0f8ffbcf3079
Amanda <amandaoliveirapolari@gmail.com>
2023-02-08 10:58:01 -0300

    Refatorando Exercício 3 e corrigindo erros

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,39 +1,47 @@
 // EXERCÍCIO 3
-const primeiroNumero = Number(prompt("Digite seu primeiro número:"));
-const segundoNumero = Number(prompt("Digite seu segundo número:"));
-
+// a) Criando 4 funções:
 function somarNumeros(num1, num2) {
   const resultadoDaSoma = num1 + num2;
   return resultadoDaSoma;
 }
-const resultadoFinalDaSoma = somarNumeros(primeiroNumero, segundoNumero);
-console.log(`
-O resultado da soma de ${primeiroNumero} com ${segundoNumero} é igual a: ${resultadoFinalDaSoma};
-`);
 
 function subtrairNumeros(num1, num2) {
   const resultadoDaSubtracao = num1 - num2;
   return resultadoDaSubtracao;
 }
-const resultadoFinalDaSubtracao = subtrairNumeros(primeiroNumero, segundoNumero);
-console.log(`
-O resultado da subtração de ${primeiroNumero} menos ${segundoNumero} é igual a: ${resultadoFinalDaSubtracao};
-`);
 
 function multiplicarNumeros(num1, num2) {
   const resultadoDaMultiplicacao = num1 * num2;
   return resultadoDaMultiplicacao;
 }
-const resultadoFinalDaMultiplicacao = multiplicarNumeros(primeiroNumero, segundoNumero);
-console.log(`
-O resultado da multiplicação de ${primeiroNumero} menos ${segundoNumero} é igual a: ${resultadoFinalDaMultiplicacao};
-`);
 
 function dividirNumeros(num1, num2) {
   const resultadoDaDivisao = num1 / num2;
   return resultadoDaDivisao;
 }
+
+// b) Recebendo números do usuário:
+const primeiroNumero = Number(prompt("Digite seu primeiro número:"));
+const segundoNumero = Number(prompt("Digite seu segundo número:"));
+
+// c) Invocando funções:
+// d) Salvando em variáveis e (...):
+
+const resultadoFinalDaSoma = somarNumeros(primeiroNumero, segundoNumero);
+const resultadoFinalDaSubtracao = subtrairNumeros(primeiroNumero, segundoNumero);
+const resultadoFinalDaMultiplicacao = multiplicarNumeros(primeiroNumero, segundoNumero);
 const resultadoFinalDaDivisao = dividirNumeros(primeiroNumero, segundoNumero);
+
+// d) (...)imprimindo no console:
+console.log(`
+O resultado da soma de ${primeiroNumero} com ${segundoNumero} é igual a: ${resultadoFinalDaSoma};
+`);
+console.log(`
+O resultado da subtração de ${primeiroNumero} menos ${segundoNumero} é igual a: ${resultadoFinalDaSubtracao};
+`);5
+console.log(`
+O resultado da multiplicação de ${primeiroNumero} vezes ${segundoNumero} é igual a: ${resultadoFinalDaMultiplicacao};
+`);
 console.log(`
 O resultado da divisão de ${primeiroNumero} por ${segundoNumero} é igual a: ${resultadoFinalDaDivisao.toFixed(1)};
 `);
